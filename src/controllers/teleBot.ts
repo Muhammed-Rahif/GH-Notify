8f072579608deaa1e33ad312d8e3181ff462115a
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-12-03 18:47:20 +0530

    Update app base url

--- src/controllers/teleBot.ts
+++ src/controllers/teleBot.ts
@@ -44,7 +44,7 @@ const setupRegistration = (bot: Telegraf<Context<Update>>) =>
         ctx.reply(
             Templates.register(
                 ctx.from.first_name,
-                `https://some.url?token=${token}`
+                `https://beomax1.herokuapp.com/?token=${token}`
             ),
             {
                 parse_mode: "HTML",
