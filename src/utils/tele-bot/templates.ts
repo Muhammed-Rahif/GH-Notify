5cc889c53b0fbf6c2526bfabb65631a6023e1943
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-12-07 21:57:15 +0530

    Update user data telegram bot command reply setup

--- src/utils/tele-bot/templates.ts
+++ src/utils/tele-bot/templates.ts
@@ -3,6 +3,8 @@ import { getReasonDesc } from "../github/reasons";
 class Templates {
     static register = (fullName: string, url: string): string =>
         `<b>༺♥༻❀༺♥༻</b>\n\nHello <b>${fullName}</b>,\nYou need to register to get started with us.!\n<b><a href="${url}">Register from here!</a></b>\n\n<b>༺♥༻❀༺♥༻</b>`;
+    static updateUser = (fullName: string, url: string): string =>
+        `<b>- ––––•(-• •-)•–––– -</b>\n\nHello <b>${fullName}</b>,\nTo update your data please click the link, there you will find a form to update your profile.!\n<b><a href="${url}">Update your data from here!</a></b>\n\n<b>◄ ◊ ►</b>`;
     static notification = (title: string, type: string, reason: string) =>
         `<b>◤▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼◥</b>\n<b>Title : ${title}</b>\nType : ${type}\nReason : ${getReasonDesc(
             reason
